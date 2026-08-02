e870863b2ca6589bc83e6369bc9109fedfe71687
amirxray <amirxray78@gmail.com>
2026-08-02 13:36:22 +0330

    Implement basic query builder

---
+++ phase_querybuilder/1/QueryBuilderDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using QueryBuilder;
+
+var query = new Query()
+    .From("Students")
+    .Select("Id", "Name")
+    .Where("IsMale", true)
+    .Where("Age", 20)
+    .Where("Name", "Ali");
+
+Console.WriteLine(query);
+
