3e071c652f6a3553a67cb8eb2fc464392d2806b7
RECYRUSED <javadjamshidi2002@gmail.com>
2026-08-18 13:45:13 +0330

    property value in condition set to [required]

--- Unit-Test/QueryLib.Demo/QueryResult.cs
+++ Unit-Test/QueryLib.Demo/QueryResult.cs
@@ -3,5 +3,4 @@ public sealed class QueryResult
 {
     public required IReadOnlyCollection<string> ColumnNames { get; init; }
     public required IReadOnlyCollection<Dictionary<string, object?>> Rows { get; init; }
-    
 }
--- Unit-Test/QueryLib/Clauses/Condition.cs
+++ Unit-Test/QueryLib/Clauses/Condition.cs
@@ -3,6 +3,6 @@ namespace QueryLib.Clauses;
 public sealed class Condition
 {
     public required string Column { get; init; }
-    public  object? Value { get; init; }
+    public required object? Value { get; init; }
 }
 
